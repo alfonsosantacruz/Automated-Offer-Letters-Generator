8bd126cce40d9905e2b6adb5ac75a707e3d84c76
Alfonso Santacruz Garcia <50888490+alfonsosantacruz@users.noreply.github.com>
2022-04-01 22:05:32 -0600

    Adds actions by column object

--- python/gsheets_functions.py
+++ python/gsheets_functions.py
@@ -3,6 +3,29 @@
 # Posts update in updates sheet
 # Inspired from postUpdatedStudentInfoAsUpdate function in the Apps Script implementation
 
+# Code of action per column in the Google Sheet
+actions_by_column = {
+  2: "StudentEmail",
+  3: "StudentFullName",
+  4: "Citizenship",
+  6: "Eligibility",
+  7: "Rotation",
+  8: "Company",
+  9: "Role",
+  10: "Manager",
+  11: "Salary",
+  12: "Project Code",
+  13: "CTD Sign Off",
+  14: "PaycomID",
+  17: "Dept Code",
+  18: "Manager Email",
+  101: "Doc_Unsigned_Offer_Link",
+  102: "PDF_Unsigned_Offer_Link",
+  103: "HelloSign_Offer_UUID",
+  104: "Signed_Offer_Link",
+  105: "Onboarding_Email_Sent"
+}
+
 def post_updated_info_as_update(updates_sheet,
                                 last_row_index,
                                 today, 
